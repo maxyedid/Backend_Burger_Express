f8b35ead96a378c240d8f4a4249744317c58222d
Max Yedid <maxyedid1@gmail.com>
2022-01-26 15:05:12 -0500

    Heroku problems

--- server/db/database.js
+++ server/db/database.js
@@ -5,11 +5,6 @@ const pkg = ('../../package.json')
 const db = new Sequelize(
     process.env.DATABASE_URL || `postgres://postgres:sql@localhost:5432/backend_burger_express`, {
     dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-            rejectUnauthorized: false
-        }
-    },
     logging: false
 })
 
