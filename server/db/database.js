7d69f86d47456b89dd15e5da6e9cc62aacc33b46
Max Yedid <maxyedid1@gmail.com>
2022-01-26 14:35:10 -0500

    Fixing heroku deployment issues

--- server/db/database.js
+++ server/db/database.js
@@ -4,7 +4,7 @@ const pkg = ('../../package.json')
 
 const db = new Sequelize(
     process.env.DATABASE_URL || `postgres://postgres:sql@localhost:5432/backend_burger_express`, {
-    host: "localhost",
+    host: "10.0.2.2",
     dialect: 'postgres',
     logging: false
 })
