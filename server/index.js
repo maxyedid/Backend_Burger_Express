5514dfb9d98e4d60b13f0408fd1ffa5e1fad7521
Max Yedid <maxyedid1@gmail.com>
2022-01-27 13:59:06 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ db.sync({force: true}).then(() => {
     FoodItem.create({name: "Cheese Burger", description: "Burger with cheese", type: "entree", price: 12.50, imageURL: "https://www.mcdonalds.com/is/image/content/dam/usa/nfl/nutrition/items/hero/desktop/t-mcdonalds-Cheeseburger.jpg?$Product_Desktop$"})
     FoodItem.create({name: "Hot Dog", type: "entree", price: 5.00, imageURL: "https://d3mvlb3hz2g78.cloudfront.net/wp-content/uploads/2014/06/thumb_720_450_dreamstime_xl_34122178-Custom.jpg"})
     FoodItem.create({name: "French Fries", type: "appetizer", price: 3.50, imageURL: "https://www.seriouseats.com/thmb/Gm6J49wltvTCH2dXt039zkvGZLg=/880x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__2018__04__20180309-french-fries-vicky-wasik-15-5a9844742c2446c7a7be9fbd41b6e27d.jpg"})
-     FoodItem.create({name: "Coca Cola", type: "drink", price: 2.00, imageURL: "https://media.istockphoto.com/photos/classic-cocacola-can-picture-id465133878?k=20&m=465133878&s=612x612&w=0&h=vgcvEz4ts5WZQOhKMaKon8mIQVbr20-JhP7DKgo_wD0="})
+    FoodItem.create({name: "Coca Cola", type: "drink", price: 2.00, imageURL: "https://media.istockphoto.com/photos/classic-cocacola-can-picture-id465133878?k=20&m=465133878&s=612x612&w=0&h=vgcvEz4ts5WZQOhKMaKon8mIQVbr20-JhP7DKgo_wD0="})
     FoodItem.create({name: "Pudding", description: "Vanilla pudding in a small cup", type: "dessert", price: 4.00, imageURL: "https://www.mybakingaddiction.com/wp-content/uploads/2021/03/vanilla-pudding-with-fruit-720x1080.jpg.webp"})
 
     app.listen(process.env.PORT || PORT, () => {
