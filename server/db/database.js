0d3160ac1a9414376ee1a5bdacc6e830bba83987
Max Yedid <maxyedid1@gmail.com>
2022-01-27 14:06:07 -0500

    Update database.js

--- server/db/database.js
+++ server/db/database.js
@@ -6,13 +6,13 @@ const db = new Sequelize(
     process.env.DATABASE_URL || `postgres://postgres:sql@localhost:5432/backend_burger_express`, {
     dialect: 'postgres',
     logging: false,
-    /*ssl: process.env.DATABASE_URL ? true : false,
+    ssl: process.env.DATABASE_URL ? true : false,
     dialectOptions: {
         ssl: {
             require: true,
             rejectUnauthorized: false
         }
-    }*/
+    }
 })
 
 module.exports = db;
